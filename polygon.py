d53359e8e8579cba8ba34f670777478e0813a767
nico-ec <64324649+nico-ec@users.noreply.github.com>
2022-07-03 00:06:51 +0200

    polygon generation

--- polygon.py
+++ polygon.py
@@ -1,5 +1,23 @@
+import random
+import time
 from vector import Vector
 
 
 def generate_polygon(vertex_count: int, min: Vector, max: Vector):
-    return
+    def sortAngle(v):
+        return
+
+    random.seed(time.time())
+
+    size = max.sub(min)
+    size.x /= 2
+    size.y /= 2
+
+    polygon = []
+    for i in range(vertex_count):
+        x = random.random() * size.x #+ (min.x + size.x)
+        y = random.random() * size.y #+ (min.y + size.y)
+        direction = -1.0 if random.random() >= 0.5 else 1.0
+        polygon.append(Vector(x * direction, y * direction))
+
+    # polygon.sort(key=)
